720aefeb7effdd0b26a48541371d83516e0ab276
Nazariy <nazar260804@gmail.com>
2023-08-08 20:07:11 +0300

    added castling

--- src/rules/kingRules.ts
+++ src/rules/kingRules.ts
@@ -14,6 +14,10 @@ export function isValidKingMove(
     return false;
   }
 
+  if (isValidKingCastle(board, pieceStart, pieceEnd, king)) {
+    return true;
+  }
+
   if (
     (pieceEnd.x === pieceStart.x + 1 && pieceEnd.y === pieceStart.y) ||
     (pieceEnd.x === pieceStart.x - 1 && pieceEnd.y === pieceStart.y) ||
@@ -24,6 +28,50 @@ export function isValidKingMove(
     (pieceEnd.x === pieceStart.x - 1 && pieceEnd.y === pieceStart.y + 1) ||
     (pieceEnd.x === pieceStart.x - 1 && pieceEnd.y === pieceStart.y - 1)
   ) {
+    return true;
+  }
+
+  return false;
+}
+
+export function isValidKingCastle(
+  board: PiecePosition[],
+  pieceStart: PieceCoordinates,
+  pieceEnd: PieceCoordinates,
+  king: PiecePosition
+) {
+
+  // check king start position
+  const kingStart = king.color === ColorEnum.WHITE ? { x: 4, y: 0 } : { x: 4, y: 7 };
+  if (pieceStart.x !== kingStart.x || pieceStart.y !== kingStart.y) {
+    return false;
+  }
+
+  // check if rooks are in place
+  const rookX = pieceEnd.x > pieceStart.x ? 7 : 0;
+  
+  const rook = board.find(
+    (piece) => piece.x === rookX && piece.y === pieceStart.y
+  );
+
+  if (rook?.piece !== PieceEnum.ROOK || rook?.color !== king.color) {
+    return false;
+  }
+
+  const castlingDirection = pieceEnd.x > pieceStart.x ? 1 : -1;
+  // check if there is nothing between king and rook
+
+  for (let i = 1; i < Math.abs(king.x - rook.x); i++) {
+    const piece = board.find(
+      (piece) => piece.x === pieceStart.x + i * castlingDirection && piece.y === pieceStart.y
+    );
+    if (piece) {
+      return false;
+    }
+  }
+
+
+  if (pieceEnd.x === pieceStart.x + 2 || pieceEnd.x === pieceStart.x - 2) {
     return true;
   }
 
--- src/rules/rules.ts
+++ src/rules/rules.ts
@@ -1,57 +1,111 @@
 import { ColorEnum, PieceEnum } from "@/utils/enums";
 import { PiecePosition, PieceCoordinates } from "../utils/types";
-import { isValidBishopMove, isValidKingMove, isValidKnightMove, isValidPawnMove, isValidQueenMove, isValidRookMove } from "./";
+import {
+  isValidBishopMove,
+  isValidKingMove,
+  isValidKnightMove,
+  isValidPawnMove,
+  isValidQueenMove,
+  isValidRookMove,
+} from "./";
 
+export function isValidMove(
+  board: PiecePosition[],
+  pieceStart: PieceCoordinates,
+  pieceEnd: PieceCoordinates
+) {
+  // if out for bounds , return false
+  if (pieceEnd.x < 0 || pieceEnd.x > 7 || pieceEnd.y < 0 || pieceEnd.y > 7) {
+    return false;
+  }
 
-export function isValidMove(board: PiecePosition[], pieceStart: PieceCoordinates, pieceEnd: PieceCoordinates) {
-        // if out for bounds , return false
-        if (pieceEnd.x < 0 || pieceEnd.x > 7 || pieceEnd.y < 0 || pieceEnd.y > 7) {
-            return false;
-        }
-
-        const piece = board.find(piece => piece.x === pieceStart.x && piece.y === pieceStart.y);
-        if (piece?.piece === PieceEnum.PAWN) {
-            return isValidPawnMove(board, pieceStart, pieceEnd);
-        }
-        if (piece?.piece === PieceEnum.KNIGHT) {
-            return isValidKnightMove(board, pieceStart, pieceEnd);
-        }
-        if (piece?.piece === PieceEnum.BISHOP) {
-            return isValidBishopMove(board, pieceStart, pieceEnd);
-        }
-        if (piece?.piece === PieceEnum.ROOK) {
-            return isValidRookMove(board, pieceStart, pieceEnd);
-        }
-        if (piece?.piece === PieceEnum.QUEEN) {
-            return isValidQueenMove(board, pieceStart, pieceEnd);
-        }
-        if (piece?.piece === PieceEnum.KING) {
-            return isValidKingMove(board, pieceStart, pieceEnd);
-        }
-        return false;
+  const piece = board.find(
+    (piece) => piece.x === pieceStart.x && piece.y === pieceStart.y
+  );
+  if (piece?.piece === PieceEnum.PAWN) {
+    return isValidPawnMove(board, pieceStart, pieceEnd);
+  }
+  if (piece?.piece === PieceEnum.KNIGHT) {
+    return isValidKnightMove(board, pieceStart, pieceEnd);
+  }
+  if (piece?.piece === PieceEnum.BISHOP) {
+    return isValidBishopMove(board, pieceStart, pieceEnd);
+  }
+  if (piece?.piece === PieceEnum.ROOK) {
+    return isValidRookMove(board, pieceStart, pieceEnd);
+  }
+  if (piece?.piece === PieceEnum.QUEEN) {
+    return isValidQueenMove(board, pieceStart, pieceEnd);
+  }
+  if (piece?.piece === PieceEnum.KING) {
+    return isValidKingMove(board, pieceStart, pieceEnd);
+  }
+  return false;
 }
-        
-export function makeMove(board: PiecePosition[], pieceStart: PieceCoordinates, pieceEnd: PieceCoordinates) {
-    const piece = board.find(piece => piece.x === pieceStart.x && piece.y === pieceStart.y);
-    if (!piece) return [...board];
 
-    const capturedPiece = board.find(piece => piece.x === pieceEnd.x && piece.y === pieceEnd.y);
-    // if there is no piece at end position, move piece to end position
-    if (!capturedPiece) {
-        piece.x = pieceEnd.x;
-        piece.y = pieceEnd.y;
-        return [...board];
-    }
+export function makeMove(
+  board: PiecePosition[],
+  pieceStart: PieceCoordinates,
+  pieceEnd: PieceCoordinates
+) {
+  const piece = board.find(
+    (piece) => piece.x === pieceStart.x && piece.y === pieceStart.y
+  );
+  if (!piece) return [...board];
 
-    // if there is a piece at end position, check colors
-    if (piece.color === capturedPiece.color) {
-        return [...board];
-    }
-   
-    // if captured, return new board with captured piece removed and moving piece moved
+  // check if player is castling
+  if (
+    piece.piece === PieceEnum.KING &&
+    Math.abs(pieceEnd.x - pieceStart.x) === 2
+  ) {
+    console.log("castling");
+    return castle(piece, board, pieceStart, pieceEnd);
+  }
 
-    const newBoard = board.filter(piece => piece.x !== pieceEnd.x || piece.y !== pieceEnd.y);
+  const capturedPiece = board.find(
+    (piece) => piece.x === pieceEnd.x && piece.y === pieceEnd.y
+  );
+  // if there is no piece at end position, move piece to end position
+  if (!capturedPiece) {
     piece.x = pieceEnd.x;
     piece.y = pieceEnd.y;
-    return [...newBoard];
+    return [...board];
+  }
+
+  // if there is a piece at end position, check colors
+  if (piece.color === capturedPiece.color) {
+    return [...board];
+  }
+
+  // if captured, return new board with captured piece removed and moving piece moved
+
+  const newBoard = board.filter(
+    (piece) => piece.x !== pieceEnd.x || piece.y !== pieceEnd.y
+  );
+  piece.x = pieceEnd.x;
+  piece.y = pieceEnd.y;
+  return [...newBoard];
+}
+
+export function castle(
+  king: PiecePosition,
+  board: PiecePosition[],
+  pieceStart: PieceCoordinates,
+  pieceEnd: PieceCoordinates
+) {
+  const rookPosition =
+    pieceEnd.x > pieceStart.x
+      ? { x: 7, y: pieceStart.y }
+      : { x: 0, y: pieceStart.y };
+
+  const rook = board.find(
+    (piece) => piece.x === rookPosition.x && piece.y === rookPosition.y
+  );
+
+  if (!rook) return [...board];
+
+  rook.x = pieceEnd.x > pieceStart.x ? 5 : 3;
+  king.x = pieceEnd.x > pieceStart.x ? 6 : 2;
+
+  return [...board];
 }
