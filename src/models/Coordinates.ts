0f1f3eed9e8313f4840bb175725b37e22243fe40
Nazariy <nazar260804@gmail.com>
2023-08-12 17:56:41 +0300

    go back to typed objects :(

--- src/models/Coordinates.ts
+++ src/models/Coordinates.ts
@@ -1,18 +1,4 @@
-export class Coordinates {
-
-    public x: number;
-    public y: number;
-
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y
-    }
-
-    public sameCoordinates(coordinates: Coordinates): boolean {
-        return this.x === coordinates.x && this.y === coordinates.y;
-    }
-
-    get isOutOfBounds(): boolean {
-        return this.x < 0 || this.x > 7 || this.y < 0 || this.y > 7;
-    }
+export type Coordinates = {
+    x: number;
+    y: number;
 }
