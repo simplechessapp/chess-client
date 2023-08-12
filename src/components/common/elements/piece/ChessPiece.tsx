9979ca58def3e3555a4ca003bd5e980fe328d334
Nazariy <nazar260804@gmail.com>
2023-08-12 15:59:58 +0300

    new chessPiece component

---
+++ src/components/common/elements/piece/ChessPiece.tsx
@@ -0,0 +1,28 @@
+import { ColorEnum, PieceEnum } from "@/utils/enums";
+import styles from "./ChessPiece.module.scss";
+import { Piece } from "@/models/Piece";
+import { ReactElement } from "react";
+
+interface ChessPieceProps {
+  piece: Piece;
+}
+
+export default function ChessPiece(props: ChessPieceProps) {
+  const className = `
+    ${styles[props.piece.color]} 
+    ${styles[props.piece.piece]}
+    ${styles["piece"]}`;
+
+  const style = {
+    transform: `translate(
+      ${props.piece.coordinates.x * 100}%, 
+      ${(7 - props.piece.coordinates.y) * 100}%)`,
+  };
+
+  return (
+    <div
+      className={className}
+      style={style}
+    ></div>
+  );
+}
