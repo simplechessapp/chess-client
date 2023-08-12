f61a379067291432cb3e1d80dcbb8f420a683e70
Nazariy <nazar260804@gmail.com>
2023-08-12 17:57:38 +0300

    move functions from classes to files

--- src/rules-v2/cellChecks.ts
+++ src/rules-v2/cellChecks.ts
@@ -2,10 +2,26 @@ import { Coordinates } from "@/models/Coordinates";
 import { Piece } from "@/models/Piece";
 
 export function hasPiece(pieces: Piece[], coordinates: Coordinates) {
-    return pieces.some((piece) => piece.sameCoordinates(coordinates));
+  return pieces.some(
+    (piece) =>
+      piece.coordinates.x === coordinates.x &&
+      piece.coordinates.y === coordinates.y
+  );
 }
 
 export function getPiece(pieces: Piece[], coordinates: Coordinates) {
-    return pieces.find((piece) => piece.sameCoordinates(coordinates));
+  return pieces.find(
+    (piece) =>
+      piece.coordinates.x === coordinates.x &&
+      piece.coordinates.y === coordinates.y
+  );
 }
 
+export function isOutOfBounds(coordinates: Coordinates): boolean {
+  return (
+    coordinates.x < 0 ||
+    coordinates.x > 7 ||
+    coordinates.y < 0 ||
+    coordinates.y > 7
+  );
+}
