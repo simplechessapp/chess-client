8aab37e86408512dc41cd8ea90ee74b49d2ab429
Nazariy <nazar260804@gmail.com>
2023-08-28 22:55:17 +0300

    remove old code

--- src/rules/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./rules";
-export * from "./pawnRules";
-export * from "./knightRules";
-export * from "./bishopRules";
-export * from "./rookRules";
-export * from "./queenRules";
-export * from "./kingRules";
